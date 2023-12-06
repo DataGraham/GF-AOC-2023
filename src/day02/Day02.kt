f13a1232a2ddfcbd88dae4c408e42837b1cbd5af
Graham First <graham.first@gmail.com>
2023-12-06 15:05:43 -0500

    Solve part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -6,16 +6,26 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day02/Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("day02/Day02")
     println("Part 1 Answer: ${part1(input)}")
-    // println("Part 2 Answer: ${part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
-// TODO: Meassure performance difference for sequences?
+// TODO: Measure performance difference for sequences?
 fun part1(input: List<String>) = input.indices
     .filter { gameIndex -> input[gameIndex].isPossibleGameString() }
     .sumOf { gameIndex -> gameIndex + 1 }
+
+fun part2(input: List<String>): Int = input.sumOf { gameString -> gameString.gamePower() }
+
+private fun String.gamePower(): Int {
+    val colourCounts = handfulsFromGame().flatMap { handful -> handful.colourCountsFromHandful() }
+    return maxByColour.keys.map { colour ->
+        colourCounts.filter { it.colour == colour }.maxOfOrNull { it.count }!!
+    }.reduce { product, count -> product * count }
+}
 
 fun String.isPossibleGameString() = handfulsFromGame().all { handfulString -> handfulString.isPossibleHandfulString() }
 
@@ -23,18 +33,15 @@ fun String.isPossibleGameString() = handfulsFromGame().all { handfulString -> ha
 private fun String.handfulsFromGame() =
     substring(startIndex = indexOf(':').takeIf { it != -1 }!! + 1).split(';')
 
-fun String.isPossibleHandfulString() =
-    colourCountsFromHandful().all { colourCountString -> colourCountString.isPossibleColourCountString() }
+fun String.isPossibleHandfulString() = colourCountsFromHandful().all { it.isPossible() }
 
-private fun String.colourCountsFromHandful() = split(',').map(String::trim)
+private fun String.colourCountsFromHandful() =
+    split(',')
+        .map { rawColourCountString -> rawColourCountString.trim().split(' ').takeIf { it.size == 2 }!! }
+        .map { (countString, colourString) -> ColourCount(colour = colourString, count = countString.toInt()) }
 
 val maxByColour = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
-fun String.isPossibleColourCountString() =
-    split(' ')
-        .takeIf { it.size == 2 }!!
-        .let { (countString, colourString) -> countString.toInt() <= maxByColour[colourString]!! }
+data class ColourCount(val colour: String, val count: Int)
 
-fun part2(input: List<String>): Int {
-    return input.size
-}
+fun ColourCount.isPossible() = count <= maxByColour[colour]!!
