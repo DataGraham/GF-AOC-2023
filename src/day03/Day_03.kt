6678f36a8c2389283135f1eee09cdcd031f84875
Graham First <graham.first@gmail.com>
2023-12-07 13:51:15 -0500

    WIP: Summing ALL numbers

--- src/day03/Day_03.kt
+++ src/day03/Day_03.kt
@@ -5,16 +5,18 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day03/Day03_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 4361)
     //check(part2(testInput) == 1)
 
     val input = readInput("day03/Day03")
-    println("Part 1 Answer: ${day02.part1(input)}")
-    println("Part 2 Answer: ${day02.part2(input)}")
+    println("Part 1 Answer: ${part1(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    return input.foldIndexed(0) { index, sum, line ->
+        sum +  Regex("([0-9]+)").findAll(line).sumOf { it.value.toInt() }
+    }.also { println("Calculated $it") }
 }
 
 fun part2(input: List<String>): Int {
