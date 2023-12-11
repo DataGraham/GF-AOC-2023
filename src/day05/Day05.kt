1f25fc440ef4d9e9c8377184f7d4f98970b1395c
Graham First <graham.first@gmail.com>
2023-12-11 15:52:20 -0500

    Implement binary search for finding mapping ranges withing a mapping

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,5 +1,6 @@
 package day05
 
+import measurePerformance
 import readInput
 import requireSubstringAfter
 import split
@@ -8,15 +9,21 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 35L)
+    check(part1LinearSearch(testInput) == 35L)
+    check(part1BinarySearch(testInput) == 35L)
     //check(part2(testInput) == 1)
 
     val input = readInput("day05/Day05")
-    println("Part 1 Answer: ${part1(input)}")
-    println("Part 2 Answer: ${part2(input)}")
+    measurePerformance(label = "Part 1 (Linear Search)", reps = 5000) { part1LinearSearch(input) }
+    measurePerformance(label = "Part 1 (Binary Search)", reps = 5000) { part1BinarySearch(input) }
+    // println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Long {
+fun part1LinearSearch(input: List<String>) = part1(input) { mapInputLinearSearch(it) }
+
+fun part1BinarySearch(input: List<String>) = part1(input) { mapInputBinarySearch(it) }
+
+fun part1(input: List<String>, mappingStrategy: Mapping.(Long) -> Long): Long {
     val seeds = input.first()
         .requireSubstringAfter(':')
         .trim()
@@ -40,9 +47,8 @@ fun part1(input: List<String>): Long {
         )
     }
     return seeds.minOf { seed ->
-        mappings.mapSeedToLocation(seed).also {
-            println("Mapped Seed $seed to location $it")
-        }
+        mappings.mapSeedToLocation(seed, mappingStrategy)
+        //.also { println("Mapped Seed $seed to location $it") }
     }
 }
 
@@ -50,20 +56,28 @@ fun part2(input: List<String>): Int {
     return input.size
 }
 
-data class MappingRange(val sourceRange: LongRange, val destDelta: Long) : Comparable<Long> {
-    override fun compareTo(other: Long): Int {
-        TODO("Not yet implemented")
-    }
-}
+data class MappingRange(val sourceRange: LongRange, val destDelta: Long)
 
 class Mapping(private val mappingRanges: List<MappingRange>) {
     private val sortedMappingRanges by lazy { mappingRanges.sortedBy { it.sourceRange.first } }
 
     // TODO: Binary search to find relevant mapping range?
-    fun map(input: Long): Long =
+    fun mapInputLinearSearch(input: Long): Long =
         mappingRanges.firstOrNull { input in it.sourceRange }
             ?.let { input + it.destDelta }
             ?: input
+
+    fun mapInputBinarySearch(input: Long): Long =
+        sortedMappingRanges.binarySearch { mappingRange ->
+            when {
+                mappingRange.sourceRange.contains(input) -> 0
+                mappingRange.sourceRange.last < input -> -1
+                else -> 1
+            }
+        }.takeIf { rangeIndex -> rangeIndex >= 0 }
+            ?.let { rangeIndex -> input + sortedMappingRanges[rangeIndex].destDelta }
+            ?: input
 }
 
-fun List<Mapping>.mapSeedToLocation(seed: Long) = fold(seed) { mappedValue, mapping -> mapping.map(mappedValue) }
+fun List<Mapping>.mapSeedToLocation(seed: Long, mappingStrategy: Mapping.(Long) -> Long) =
+    fold(seed) { mappedValue, mapping -> mapping.mappingStrategy(mappedValue) }
