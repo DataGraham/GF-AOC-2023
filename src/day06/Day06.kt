39839eacab9cf83b68e15ecefff00b12e3203c16
Graham First <graham.first@gmail.com>
2023-12-14 13:40:30 -0500

    [Day 06] Blank slate for Day 06

---
+++ src/day06/Day06.kt
@@ -0,0 +1,23 @@
+package day06
+
+import readInput
+
+fun main() {
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day06/Day06_test")
+    check(part1(testInput) == 1)
+    //check(part2(testInput) == 1)
+
+    val input = readInput("day06/Day06")
+    println("Part 1 Answer: ${part1(input)}")
+    println("Part 2 Answer: ${part2(input)}")
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
