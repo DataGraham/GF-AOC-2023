79591193a45e4ee53727dd7ca4606cffd0039e96
Graham First <graham.first@gmail.com>
2023-12-14 15:02:23 -0500

    Basic parsing of hands

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -1,12 +1,12 @@
 package day07
 
+import println
 import readInput
 
 fun main() {
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day07/Day07_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 6440)
     //check(part2(testInput) == 1)
 
     val input = readInput("day07/Day07")
@@ -15,9 +15,16 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
+    val hands = input.map { line ->
+        line.split(' ').let { (cardsString, bidString) ->
+            Hand(cards = cardsString, bid = bidString.toInt())
+        }
+    }.apply { joinToString(separator = "\n").println() }
     return input.size
 }
 
 fun part2(input: List<String>): Int {
     return input.size
 }
+
+data class Hand(val cards: String, val bid: Int)
