912843a382609a2033d2451dc15ef485c8f3986c
Graham First <graham.first@gmail.com>
2023-12-16 21:52:10 -0500

    Solved part 2 and then cleaned up a bit
    Massive optimization (which is required to avoid overflowing Int index
    into sequence), is to compute the path to an endpoint (ends with Z)
    independently and then take the lcm (which is the point at which all
    paths would be at an endpoint).

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -1,16 +1,14 @@
 package day08
 
-import println
 import readInput
 import toInfiniteSequence
 import java.lang.IllegalArgumentException
+import kotlin.math.pow
 
 fun main() {
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day08/Day08_test")
-    check(part1(testInput) == 6)
-    //check(part2(testInput) == 1)
+    check(part1(readInput("day08/Day08_test")) == 6)
+    check(part2(readInput("day08/Day08_test2")) == 6L)
 
     val input = readInput("day08/Day08")
     println("Part 1 Answer: ${part1(input)}")
@@ -25,22 +23,55 @@ fun part1(input: List<String>): Int {
     return locations.indexOf("ZZZ")
 }
 
-fun part2(input: List<String>): Int {
+fun part2(input: List<String>): Long {
     val networkConnectionsByStart = getNetworkConnectionsByStart(input)
     val initialLocations = networkConnectionsByStart.keys.filter { it.last() == 'A' }
+    val endlessDirections = input.getEndlessDirections()
     val pathLengths = initialLocations.map { initialLocation ->
-        input.getEndlessDirections().scan(initialLocation) { currentLocation, direction ->
+        endlessDirections.scan(initialLocation) { currentLocation, direction ->
             networkConnectionsByStart.move(from = currentLocation, direction = direction)
         }.indexOfFirst { it.last() == 'Z' }
-    }.also { it.println() }
-    // TODO: Generate a sequence of prime numbers:
-    //  https://www.tutorialspoint.com/kotlin-program-to-display-all-prime-numbers-from-1-to-n
-    //  Or more complex but faster, for large numbers anyway: https://en.wikipedia.org/wiki/AKS_primality_test
-    // TODO: Compute the prime factorization of each path length
-    // TODO: For each prime factor that appears, take only the maximum power
-    // TODO: Return the product of these powers (the LCM)
-    return 0
+    }
+    return lcm(pathLengths)
 }
+
+private fun lcm(numbers: List<Int>) = numbers
+    // Compute the prime factorization of each path length
+    .flatMap { it.primeFactorization() }
+    .groupBy { it.prime }
+    .values
+    // For each prime factor that appears, take only the maximum power
+    .map { powersOfThisPrime -> powersOfThisPrime.maxBy { it.exponent } }
+    .map { it.prime.toDouble().pow(it.exponent.toDouble()).toLong() }
+    // Return the product of these powers (the LCM)
+    .reduce(Long::times)
+
+fun Int.primeFactorization(): Sequence<PrimeFactor> {
+    // val sqrt = floor(sqrt(this.toFloat()))
+    return primeNumbers
+        .takeWhile { it <= this }
+        .mapNotNull { primeFactor ->
+            factorExponent(primeFactor)
+                .takeIf { it != 0 }
+                ?.let { exponent -> PrimeFactor(prime = primeFactor, exponent = exponent) }
+        }
+}
+
+private val positiveIntegers = generateSequence(1) { it + 1 }
+
+// Generate a sequence of prime numbers:
+//  https://www.tutorialspoint.com/kotlin-program-to-display-all-prime-numbers-from-1-to-n
+// TODO: Or more complex but faster, for large numbers anyway: https://en.wikipedia.org/wiki/AKS_primality_test
+private val primeNumbers = positiveIntegers.filter { it.isPrime }
+
+private val Int.isPrime get() = this > 1 && (2 .. (this / 2)).all { divisor -> this % divisor != 0 }
+
+private fun Int.factorExponent(factor: Int): Int =
+    powersOf(factor).takeWhile { power -> power <= this && this % power == 0 }.count()
+
+private fun powersOf(base: Int) = generateSequence(base) { it * base }
+
+data class PrimeFactor(val prime: Int, val exponent: Int)
 
 private fun List<String>.getEndlessDirections() = first().toList().toInfiniteSequence()
 
@@ -64,7 +95,7 @@ data class NetworkConnection(
     val right: String
 ) {
     companion object {
-        private val connectionRegex = Regex("""([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)""")
+        private val connectionRegex = Regex("""([A-Z0-9]+) = \(([A-Z0-9]+), ([A-Z0-9]+)\)""")
 
         fun parseFromString(line: String) = connectionRegex
             .matchEntire(line)!!
