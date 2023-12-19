22aadc078d380125e76a7518ae91d2b743e855e5
Graham First <graham.first@gmail.com>
2023-12-19 18:17:08 -0500

    Refactor

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -23,10 +23,14 @@ fun part2(input: List<String>): Int {
 }
 
 private fun predictNextValue(line: String): Int {
-    val history = line.split(' ').map(String::toInt)
-    val derivatives = generateSequence(history) { it.differences() }
-        .takeWhile { derivative -> !derivative.all { it == 0 } }
+    val history = parseHistory(line)
+    val derivatives = history.getDerivatives()
     return derivatives.sumOf { it.last() }
 }
 
-private fun Collection<Int>.differences() = zip(drop(1)).map { (first, second) -> second - first }
+private fun List<Int>.getDerivatives() = generateSequence(this) { it.differences() }
+    .takeWhile { derivative -> !derivative.all { it == 0 } }
+
+private fun parseHistory(line: String) = line.split(' ').map(String::toInt)
+
+private fun List<Int>.differences() = zip(drop(1)).map { (first, second) -> second - first }
