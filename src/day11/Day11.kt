e5158e5558d1e574b62416cb7a0019e42a7d4717
Graham First <graham.first@gmail.com>
2023-12-25 18:37:52 -0500

    WIP: Expanding empty rows

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,5 +1,6 @@
 package day11
 
+import println
 import readInput
 
 fun main() {
@@ -8,15 +9,28 @@ fun main() {
     check(part1(testInput) == 1)
     //check(part2(testInput) == 1)
 
-    val input = readInput("day11/Day11")
-    println("Part 1 Answer: ${part1(input)}")
-    println("Part 2 Answer: ${part2(input)}")
+//    val input = readInput("day11/Day11")
+//    println("Part 1 Answer: ${part1(input)}")
+//    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
+    // Expand the galaxy
+    // 1. Replace each empty row with two empty rows
+    // 2. Detect empty columns, then, in each row, replace characters in those columns with two copies.
+
+    val expandedRows = input.flatMap { row ->
+        List(if (row.all { it == EMPTY }) 2 else 1) { row }
+    }
+
+    expandedRows.joinToString(separator = "\n").println()
+
     return input.size
 }
 
 fun part2(input: List<String>): Int {
     return input.size
 }
+
+const val EMPTY = '.'
+const val GALAXY = '#'
