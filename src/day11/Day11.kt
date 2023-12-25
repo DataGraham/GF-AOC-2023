c216a3b8170d41ba45aa0ac6b7f49e3f708e9ddb
Graham First <graham.first@gmail.com>
2023-12-25 18:23:02 -0500

    [Day 11] Setup

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,17 +1,16 @@
-/*
-package dayXX
+package day11
 
 import readInput
 
 fun main() {
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("dayXX/DayXX_test")
+    val testInput = readInput("day11/Day11_test")
     check(part1(testInput) == 1)
     //check(part2(testInput) == 1)
 
-    val input = readInput("dayXX/DayXX")
-    println("Part 1 Answer: ${day02.part1(input)}")
-    println("Part 2 Answer: ${day02.part2(input)}")
+    val input = readInput("day11/Day11")
+    println("Part 1 Answer: ${part1(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
@@ -21,4 +20,3 @@ fun part1(input: List<String>): Int {
 fun part2(input: List<String>): Int {
     return input.size
 }
-*/
