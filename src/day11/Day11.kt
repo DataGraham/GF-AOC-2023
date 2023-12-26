9377ca53022b9ee026dcebbbe0d37d87d9cd81cd
Graham First <graham.first@gmail.com>
2023-12-26 17:18:07 -0500

    Part 2 but it's wrong (smaller)
    Maybe using Long isn't enough?

--- src/day11/Day11.kt
+++ src/day11/Day11.kt
@@ -1,8 +1,10 @@
 package day11
 
+import dropLast
 import println
 import readInput
 import kotlin.math.abs
+import kotlin.math.exp
 
 fun main() {
     // test if implementation meets criteria from the description, like:
@@ -11,7 +13,7 @@ fun main() {
 
     val input = readInput("day11/Day11")
     println("Part 1 Answer: ${part1(input)}")
-    //    println("Part 2 Answer: ${part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
@@ -23,14 +25,33 @@ fun part1(input: List<String>): Int {
     }
 }
 
-fun part2(input: List<String>): Int {
+fun part2(input: List<String>): Long {
     // TODO: Yeah, we're going to have to perform expansion on the galaxy locations,
     //  rather than the entire input.
-    return input.size
+    val emptyRowsBefore = input.dropLast(1).scan(0) { before, row ->
+        if (row.isEmptyRow) before + 1 else before
+    }
+    val emptyColumnsBefore = input.first().indices.dropLast(1).scan(0) { before, col ->
+        if (input.isColumnEmpty(col)) before + 1 else before
+    }
+    val galaxyLocations = galaxyLocations(input)
+    val expandedGalaxyLocations = galaxyLocations.map { (row, col) ->
+        // TODO: Reduce multiplications by including it above (as an offset rather than empty-before count)
+        row + (emptyRowsBefore[row] * (EXPANSION_FACTOR - 1)) to
+            col + (emptyColumnsBefore[col] * (EXPANSION_FACTOR - 1))
+    }
+    return expandedGalaxyLocations.indices.sumOf { galaxyIndex ->
+        galaxyLocations[galaxyIndex].let { galaxy ->
+            galaxyLocations.drop(galaxyIndex + 1).sumOf { otherGalaxy ->
+                (galaxy manhattanDistanceTo otherGalaxy).toLong()
+            }
+        }
+    }
 }
 
 const val EMPTY_CHARACTER = '.'
 const val GALAXY_CHARACTER = '#'
+const val EXPANSION_FACTOR = 1000000
 
 fun expand(universe: List<String>): List<String> {
     // Expand the galaxy
