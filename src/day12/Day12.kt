867cf5002a783d3e50cdc998fdf1e92505f08d60
Graham First <graham.first@gmail.com>
2023-12-30 00:40:39 -0500

    At least we pass the part 2 check, but it's still too slow

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -10,7 +10,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day12/Day12_test")
     check(part1(testInput).also { it.println() } == 21)
-    // check(part2(testInput) == 1)
+    check(part2(testInput).also { it.println() } == 525152)
 
     val input = readInput("day12/Day12")
     println("Part 1 Answer: ${part1(input)}")
@@ -20,21 +20,28 @@ fun main() {
 fun part1(input: List<String>) = input.sumOf { line -> arrangementCount(line) }
 
 fun part2(input: List<String>): Int {
+    var done = 0
     return input.sumOf { line ->
+        /*
         val lineInfo = parseLine(line)
         val unfoldedLineInfo = LineInfo(
             working = List(5) { lineInfo.working }.flatten(),
             nonWorkingRuns = List(5) { lineInfo.nonWorkingRuns }.flatten()
         )
-        arrangementCount(unfoldedLineInfo)
+        */
+        val unfoldedLineInfo = unfoldAndParseLine(line)
+        arrangementCount(unfoldedLineInfo).also {
+            println("Done ${++done}")
+        }
     }
 }
 
 fun arrangementCount(line: String): Int {
     // line.println()
     val lineInfo = parseLine(line)
-    val arrangements = nonWorkingRunStarts(lineInfo)
+    return arrangementCount(lineInfo)
     /*
+    val arrangements = nonWorkingRunStarts(lineInfo)
     arrangements.forEach { nonWorkingRunStarts ->
         val states = lineInfo.working.toMutableList()
         nonWorkingRunStarts.forEachIndexed { runIndex, nonWorkingRunStart ->
@@ -51,8 +58,8 @@ fun arrangementCount(line: String): Int {
         }
         // statesString.println()
     }
-    */
     return arrangements.size
+    */
 }
 
 fun arrangementCount(lineInfo: LineInfo): Int {
@@ -62,15 +69,17 @@ fun arrangementCount(lineInfo: LineInfo): Int {
     if (lineInfo.nonWorkingRuns.isEmpty()) {
         return if (false in lineInfo.working) 0 else 1
     }
+    val minRemainderSpace = lineInfo.nonWorkingRuns.drop(1).run { sum() + size } // TODO: or sumOf { it + 1 } ?
     val firstRunPositions = possibleStartPositions(
         // Non-working run length,
         nonWorkingRunLength = lineInfo.nonWorkingRuns.first(),
         // state slice
-        working = lineInfo.working
+        working = lineInfo.working,
         // TODO: This makes it more efficient, but we generate invalid positions
         //  because we can't see that the remainder might start with a known false (broken/#)
         //  BUT can I still pass this in as an upper-bound of what to consider?
-        // .dropLast(lineInfo.nonWorkingRuns.drop(1).run { sum() + size })
+        // .dropLast(minRemainderSpace)
+        upperBoundEndPosition = lineInfo.working.lastIndex - minRemainderSpace
     )
     return firstRunPositions.sumOf { firstRunPosition ->
         arrangementCount(
@@ -92,11 +101,13 @@ fun nonWorkingRunStarts(lineInfo: LineInfo): List<List<Int>> {
         else
             listOf(emptyList()) // there is exactly one arrangement with no non-working runs
     }
+    val minRemainderSpace = lineInfo.nonWorkingRuns.drop(1).run { sum() + size } // TODO: or sumOf { it + 1 } ?
     val firstRunPositions = possibleStartPositions(
         // Non-working run length,
         nonWorkingRunLength = lineInfo.nonWorkingRuns.first(),
         // state slice
-        working = lineInfo.working
+        working = lineInfo.working,
+        upperBoundEndPosition = lineInfo.working.lastIndex - minRemainderSpace
         // TODO: This makes it more efficient, but we generate invalid positions
         //  because we can't see that the remainder might start with a known false (broken/#)
         //  BUT can I still pass this in as an upper-bound of what to consider?
@@ -121,10 +132,10 @@ private fun Boolean?.toChar() = when (this) {
     null -> "?"
 }
 
-fun possibleStartPositions(nonWorkingRunLength: Int, working: List<Boolean?>): List<Int> {
+fun possibleStartPositions(nonWorkingRunLength: Int, working: List<Boolean?>, upperBoundEndPosition: Int): List<Int> {
     // println("Within ${working.joinToString(separator = "") { it.toChar() }}, a run of $nonWorkingRunLength...")
     // Slide across, and it's valid IFF:
-    return (0..working.size - nonWorkingRunLength).filter { hypotheticalStart ->
+    return (0..(upperBoundEndPosition + 1) - nonWorkingRunLength).filter { hypotheticalStart ->
         // Is it valid (true/false):
 
         //  Everything before, if any, is not false
@@ -149,6 +160,19 @@ private fun parseLine(line: String) =
     line.split(' ')
         .takeIf { it.size == 2 }!!
         .let { (statesString, runsString) ->
+            LineInfo(
+                working = statesString.map { char -> char.toWorkingState() },
+                nonWorkingRuns = runsString.split(',').map(String::toInt)
+            )
+        }
+
+private fun unfoldAndParseLine(line: String) =
+    line.split(' ')
+        .takeIf { it.size == 2 }!!
+        .let { (statesString, runsString) ->
+            List(5) { statesString }.joinToString(separator = "?") to
+                List(5) { runsString }.joinToString(separator = ",")
+        }.let { (statesString, runsString) ->
             LineInfo(
                 working = statesString.map { char -> char.toWorkingState() },
                 nonWorkingRuns = runsString.split(',').map(String::toInt)
