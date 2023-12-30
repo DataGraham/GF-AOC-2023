833ba49d3824097b14d012f254136a2156dc7e09
Graham First <graham.first@gmail.com>
2023-12-29 22:58:50 -0500

    It passes the test!

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -6,7 +6,7 @@ import readInput
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day12/Day12_test")
-    check(part1(testInput) == 21)
+    check(part1(testInput).also { it.println() } == 21)
     //check(part2(testInput) == 1)
 
     val input = readInput("day12/Day12")
@@ -14,20 +14,53 @@ fun main() {
     println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Int {
-    return input.sumOf { line ->
-        arrangementCount(line)
-    }
-}
+fun part1(input: List<String>) = input.sumOf { line -> arrangementCount(line) }
 
 fun part2(input: List<String>): Int {
     return input.size
 }
 
-fun arrangementCount(line: String): Int {
-    val lineInfo = parseLine(line)
-    lineInfo.println()
-    return 0
+fun arrangementCount(line: String) = arrangementCount(parseLine(line))
+
+fun arrangementCount(lineInfo: LineInfo): Int {
+    // Recursion base case:
+    // Once there are no more broken runs to place,
+    // there simply must be no more definitely-broken states.
+    if (lineInfo.nonWorkingRuns.isEmpty()) {
+        return if (false in lineInfo.working) 0 else 1
+    }
+    val firstRunPositions = possibleStartPositions(
+        // Non-working run length,
+        nonWorkingRunLength = lineInfo.nonWorkingRuns.first(),
+        // state slice
+        working = lineInfo.working.dropLast(
+            lineInfo.nonWorkingRuns.drop(1).run { sum() + size }
+        )
+    )
+    return firstRunPositions.sumOf { firstRunPosition ->
+        arrangementCount(
+            LineInfo(
+                working = lineInfo.working.drop(firstRunPosition + lineInfo.nonWorkingRuns.first() + 1),
+                nonWorkingRuns = lineInfo.nonWorkingRuns.drop(1)
+            )
+        )
+    }
+}
+
+fun possibleStartPositions(nonWorkingRunLength: Int, working: List<Boolean?>): List<Int> {
+    // Slide across, and it's valid IFF:
+    return (0..working.size - nonWorkingRunLength).filter { hypotheticalStart ->
+        // Is it valid (true/false):
+
+        //  Everything before, if any, is not false
+        (0 ..< hypotheticalStart).none { working[it] == false } &&
+            //  everything within the run is not true
+            (hypotheticalStart ..< hypotheticalStart + nonWorkingRunLength).none { working[it] == true } &&
+            //  The space after (if it exists), is not false
+            (hypotheticalStart + nonWorkingRunLength).let { positionAfter ->
+                positionAfter > working.lastIndex || working[positionAfter] != false
+            }
+    }
 }
 
 data class LineInfo(
@@ -45,7 +78,7 @@ private fun parseLine(line: String) =
             )
         }
 
-private fun Char.toWorkingState() = when(this) {
+private fun Char.toWorkingState() = when (this) {
     '.' -> true
     '#' -> false
     '?' -> null
