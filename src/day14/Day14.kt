642bd797c57327b6e753243f7a55b4cdc7563f7b
Graham First <graham.first@gmail.com>
2024-01-01 16:58:26 -0500

    Solved part 1

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -7,22 +7,20 @@ import transposed
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day14/Day14_test")
-    check(part1(testInput).also { it.println() } == 1)
+    check(part1(testInput).also { it.println() } == 136)
     //check(part2(testInput).also { it.println() } == 1)
 
-//    val input = readInput("day14/Day14")
-//    println("Part 1 Answer: ${part1(input)}")
-//    println("Part 2 Answer: ${part2(input)}")
+    val input = readInput("day14/Day14")
+    println("Part 1 Answer: ${part1(input)}")
+    // println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
     // Make a string for each column
     val columns = input.transposed()
-    val tiltedColumns = columns.map(String::tiltedNorth).also { it.forEach { it.println() } }
-
+    val tiltedColumns = columns.map(String::tiltedNorth)
     // For each string, sum for each 'O' its index from the end + 1
-
-    return input.size
+    return tiltedColumns.sumOf(String::load)
 }
 
 fun part2(input: List<String>): Int {
@@ -50,3 +48,9 @@ private fun String.tiltedNorth(): String {
     // Now put the substrings back together
     return tiltedAreas.joinToString(separator = "")
 }
+
+private fun String.load() =
+    withIndex().sumOf { (index, char) ->
+        if (char == ROUND_ROCK) length - index
+        else 0
+    }
