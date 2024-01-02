ce7bbb52ec40da97620d0c9a94585bf85b45822d
Graham First <graham.first@gmail.com>
2024-01-02 16:41:30 -0500

    Reorder test cases

--- src/day14/TestFindCycle.kt
+++ src/day14/TestFindCycle.kt
@@ -5,23 +5,6 @@ import org.junit.jupiter.api.Test
 
 class TestFindCycle {
     @Test
-    fun testFindTypicalCycle() {
-        val nextIndex = listOf(1, 2, 3, 4, 5, 2)
-        /*
-        0 -> 1 -> 2 -> 3 -> 4 -> 5
-                  ^              |
-                  ---------------
-        */
-        val expectedCycle = CycleInfo(
-            distanceToStart = 2,
-            startValue = 2,
-            length = 4
-        )
-        val cycle = findCycle(0) { nextIndex[this] }
-        assertThat(cycle).isEqualTo(expectedCycle)
-    }
-
-    @Test
     fun findMinimalCycle() {
         val expectedCycle = CycleInfo(
             distanceToStart = 0,
@@ -40,6 +23,23 @@ class TestFindCycle {
             length = 2
         )
         val cycle = findCycle(false) { !this }
+        assertThat(cycle).isEqualTo(expectedCycle)
+    }
+
+    @Test
+    fun testFindTypicalCycle() {
+        val nextIndex = listOf(1, 2, 3, 4, 5, 2)
+        /*
+        0 -> 1 -> 2 -> 3 -> 4 -> 5
+                  ^              |
+                  ---------------
+        */
+        val expectedCycle = CycleInfo(
+            distanceToStart = 2,
+            startValue = 2,
+            length = 4
+        )
+        val cycle = findCycle(0) { nextIndex[this] }
         assertThat(cycle).isEqualTo(expectedCycle)
     }
 }
