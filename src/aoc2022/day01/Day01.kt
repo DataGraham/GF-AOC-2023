eb9cb9f95a158ee01d57e10f5a93929f8f9ee7a9
Graham First <graham.first@gmail.com>
2024-11-01 19:03:39 -0400

    Solved part 2

--- src/aoc2022/day01/Day01.kt
+++ src/aoc2022/day01/Day01.kt
@@ -8,20 +8,18 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2022/day01/Day01_test")
     check(part1(testInput).also { it.println() } == 24000)
-    //check(part2(testInput).also { it.println() } == 1)
+    check(part2(testInput).also { it.println() } == 45000)
 
     val input = readInput("aoc2022/day01/Day01")
     println("Part 1 Answer: ${part1(input)}")
-    //println("Part 2 Answer: ${part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>) = input.toElves().maxOf(Elf::totalCalories)
 
 private val Elf.totalCalories get() = foodItems.sumOf { it.calories }
 
-fun part2(input: List<String>): Int {
-    return input.size
-}
+fun part2(input: List<String>) = input.toElves().map { it.totalCalories }.sorted().takeLast(3).sum()
 
 data class FoodItem(val calories: Int)
 
