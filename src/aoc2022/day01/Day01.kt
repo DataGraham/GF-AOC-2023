864fc825fc17a07146f78a1ee7d2f5ff63fab11f
Graham First <graham.first@gmail.com>
2024-11-01 18:59:11 -0400

    Refactor

--- src/aoc2022/day01/Day01.kt
+++ src/aoc2022/day01/Day01.kt
@@ -7,7 +7,7 @@ import split
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2022/day01/Day01_test")
-    check(part1(testInput).also { it.println() } == 24000 )
+    check(part1(testInput).also { it.println() } == 24000)
     //check(part2(testInput).also { it.println() } == 1)
 
     val input = readInput("aoc2022/day01/Day01")
@@ -15,13 +15,26 @@ fun main() {
     //println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Int {
-    val elves = input.split { it.isBlank() }
-    return elves.maxOf { elfItems ->
-        elfItems.sumOf { itemCaloriesString -> itemCaloriesString.toInt() }
-    }
-}
+fun part1(input: List<String>) = input.toElves().maxOf(Elf::totalCalories)
+
+private val Elf.totalCalories get() = foodItems.sumOf { it.calories }
 
 fun part2(input: List<String>): Int {
     return input.size
 }
+
+data class FoodItem(val calories: Int)
+
+data class Elf(val foodItems: List<FoodItem>)
+
+fun List<String>.toElves() =
+    split { it.isBlank() }
+        .map { elfCalorieStrings ->
+            elfCalorieStrings.toElf()
+        }
+
+private fun List<String>.toElf() = Elf(
+    foodItems = map { caloriesString ->
+        FoodItem(calories = caloriesString.toInt())
+    }
+)
