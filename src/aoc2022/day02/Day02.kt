1f2b86ba6074624a2d5e5fbfb884d8d6bbfbd3a0
Graham First <graham.first@gmail.com>
2024-11-02 19:13:36 -0400

    Solve part 2

--- src/aoc2022/day02/Day02.kt
+++ src/aoc2022/day02/Day02.kt
@@ -2,7 +2,8 @@ package aoc2022.day02
 
 import aoc2022.day02.Hand.*
 import aoc2022.day02.RoundResult.*
-import aoc2022.day02.StrategyDecoder.toRound
+import aoc2022.day02.StrategyDecoder.toRoundPart1
+import aoc2022.day02.StrategyDecoder.toRoundPart2
 import println
 import readInput
 import require
@@ -11,18 +12,16 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2022/day02/Day02_test")
     check(part1(testInput).also { it.println() } == 15)
-    //check(part2(testInput).also { it.println() } == 1)
+    check(part2(testInput).also { it.println() } == 12)
 
     val input = readInput("aoc2022/day02/Day02")
     println("Part 1 Answer: ${part1(input)}")
-    //println("Part 2 Answer: ${day02.part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>) = input.sumOf { it.toRound().score }
+fun part1(input: List<String>) = input.sumOf { it.toRoundPart1().score }
 
-fun part2(input: List<String>): Int {
-    return input.size
-}
+fun part2(input: List<String>) = input.sumOf { it.toRoundPart2().score }
 
 data class Round(val opponentHand: Hand, val selfHand: Hand)
 
@@ -42,19 +41,36 @@ enum class RoundResult(val score: Int) {
 }
 
 object StrategyDecoder {
-    fun String.toRound(): Round {
-        val (opponentCode, selfCode) = split(' ')
-            .require { size == 2 }
-            .require { all { it.length == 1 } }
-            .map { it.first() }
+    fun String.toRoundPart1(): Round {
+        val (opponentCode, selfCode) = extractCodes()
         return Round(
             opponentHand = opponentCode.toOpponentHand(),
             selfHand = selfCode.toSelfHand()
         )
     }
 
+    fun String.toRoundPart2(): Round {
+        val (opponentCode, resultCode) = extractCodes()
+        val opponentHand = opponentCode.toOpponentHand()
+        return Round(
+            opponentHand = opponentHand,
+            selfHand = when(resultCode.toResult()) {
+                Win -> Hand.entries.first { it beats opponentHand }
+                Lose -> Hand.entries.first { opponentHand beats it }
+                Tie -> opponentHand
+            }
+        )
+    }
+
+    private fun String.extractCodes() = split(' ')
+        .require { size == 2 }
+        .require { all { it.length == 1 } }
+        .map { codeString -> codeString.first() }
+        .let { codeCharacters -> codeCharacters[0] to codeCharacters[1] }
+
     private fun Char.toOpponentHand() = opponentHandsByCode[this]!!
     private fun Char.toSelfHand() = selfHandsByCode[this]!!
+    private fun Char.toResult() = resultsByCode[this]!!
 
     private val opponentHandsByCode = mapOf(
         'A' to Rock,
@@ -68,6 +84,11 @@ object StrategyDecoder {
         'Z' to Scissors
     )
 
+    private val resultsByCode = mapOf(
+        'X' to Lose,
+        'Y' to Tie,
+        'Z' to Win
+    )
 }
 
 enum class Hand(val intrinsicScore: Int) {
