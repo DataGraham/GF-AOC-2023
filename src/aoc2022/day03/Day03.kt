7c6250b3e4df179bcbb0a8edadd4b10079637a9c
Graham First <graham.first@gmail.com>
2024-11-04 18:34:52 -0500

    [Day 03] Setup

---
+++ src/aoc2022/day03/Day03.kt
@@ -0,0 +1,23 @@
+package aoc2022.day
+
+import println
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("aoc2022/day03/Day03_test")
+    check(part1(testInput).also { it.println() } == 157)
+    //check(part2(testInput).also { it.println() } == 1)
+
+//    val input = readInput("aoc2022/day03/Day03")
+//    println("Part 1 Answer: ${day02.part1(input)}")
+//    println("Part 2 Answer: ${day02.part2(input)}")
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
