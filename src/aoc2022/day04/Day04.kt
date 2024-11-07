48cda890a64c8b2c5ec3b57eeaea26bb1eaf9953
Graham First <graham.first@gmail.com>
2024-11-06 22:14:20 -0500

    Solve part 2

--- src/aoc2022/day04/Day04.kt
+++ src/aoc2022/day04/Day04.kt
@@ -8,17 +8,22 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2022/day04/Day04_test")
     check(part1(testInput).also { it.println() } == 2)
-    //check(part2(testInput).also { it.println() } == 1)
+    check(part2(testInput).also { it.println() } == 4)
 
     val input = readInput("aoc2022/day04/Day04")
     println("Part 1 Answer: ${part1(input)}")
-    //    println("Part 2 Answer: ${part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>) =
     input
         .map { line -> line.toSectionIdRanges() }
-        .count { sectionIdRanges -> sectionIdRanges.oneContainsTheOther() }
+        .count { sectionIdRangePair -> sectionIdRangePair.oneContainsTheOther() }
+
+fun part2(input: List<String>) =
+    input
+        .map { line -> line.toSectionIdRanges() }
+        .count { sectionIdRangePair -> sectionIdRangePair.first overlaps sectionIdRangePair.second }
 
 fun String.toSectionIdRanges(): Pair<IntRange, IntRange> =
     split(',')
@@ -35,9 +40,8 @@ private fun String.toSectionIdRange() =
 private fun Pair<IntRange, IntRange>.oneContainsTheOther() =
     first in second || second in first
 
+private infix fun IntRange.overlaps(other: IntRange) =
+    first <= other.last && last >= other.first
+
 operator fun IntRange.contains(other: IntRange) =
     first <= other.first && last >= other.last
-
-fun part2(input: List<String>): Int {
-    return input.size
-}
