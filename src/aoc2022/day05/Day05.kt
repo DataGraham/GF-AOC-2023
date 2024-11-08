239614e9ce1a0195102588c4ec936d7328d969e5
Graham First <graham.first@gmail.com>
2024-11-07 20:51:51 -0500

    [Day 05] Setup

---
+++ src/aoc2022/day05/Day05.kt
@@ -0,0 +1,23 @@
+package aoc2022.day05
+
+import println
+import readInput
+
+fun main() {
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("aoc2022/day05/Day05_test")
+    check(part1(testInput).also { it.println() } == "CMZ")
+    //check(part2(testInput).also { it.println() } == 4)
+
+    //val input = readInput("aoc2022/day05/Day05")
+    //println("Part 1 Answer: ${part1(input)}")
+    //println("Part 2 Answer: ${part2(input)}")
+}
+
+fun part1(input: List<String>): String {
+    return ""
+}
+
+fun part2(input: List<String>): String {
+    return ""
+}
