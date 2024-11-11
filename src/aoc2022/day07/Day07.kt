d00d31b4caf1d39f204cc47ae0a00b0f985eea3b
Graham First <graham.first@gmail.com>
2024-11-11 18:51:11 -0500

    Add classes to represent different types of terminal lines

--- src/aoc2022/day07/Day07.kt
+++ src/aoc2022/day07/Day07.kt
@@ -14,6 +14,18 @@ fun main() {
     //println("Part 2 Answer: ${aoc2022.day07.part2(input)}")
 }
 
+private sealed class TerminalLine {
+    sealed class Command : TerminalLine() {
+        data class ChangeDirectory(val directoryName: String) : Command()
+        data object List : Command()
+    }
+
+    sealed class Output : TerminalLine() {
+        data class File(val name: String, val size: Int) : Output()
+        data class Directory(val name: String) : Output()
+    }
+}
+
 fun part1(input: List<String>): Int {
     return input.size
 }
