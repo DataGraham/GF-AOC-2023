4ac5dd2f7c7d413c67c08cbbdb52aa360d3caf62
Graham First <graham.first@gmail.com>
2024-11-13 18:12:42 -0500

    Part 1 solved!

--- src/aoc2022/day07/Day07.kt
+++ src/aoc2022/day07/Day07.kt
@@ -17,8 +17,8 @@ fun main() {
     check(part1(testInput).also { it.println() } == 95437)
     //check(aoc2022.day07.part2(testInput).also { it.println() } == 1)
 
-    //val input = readInput("aoc2022/day07/Day07")
-    //println("Part 1 Answer: ${aoc2022.day07.part1(input)}")
+    val input = readInput("aoc2022/day07/Day07")
+    println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${aoc2022.day07.part2(input)}")
 }
 
