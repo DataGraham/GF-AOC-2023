3f40d7a589dd6e4ecdfffbe7a47e2cdff075f962
Graham First <graham.first@gmail.com>
2024-11-15 19:04:23 -0500

    Solved part 1

--- src/aoc2022/day08/Day08.kt
+++ src/aoc2022/day08/Day08.kt
@@ -1,5 +1,7 @@
 package aoc2022.day08
 
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
 import println
 import readInput
 
@@ -9,8 +11,8 @@ fun main() {
     check(part1(testInput).also { it.println() } == 21)
     //check(part2(testInput).also { it.println() } == 24933642)
 
-    //val input = readInput("aoc2022/day08/Day08")
-    //println("Part 1 Answer: ${part1(input)}")
+    val input = readInput("aoc2022/day08/Day08")
+    println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${part2(input)}")
 }
 
@@ -18,8 +20,61 @@ fun part1(input: List<String>): Int {
     val treeRows = input.map { line ->
         line.map { it.digitToInt() }
     }
-
-    return input.size
+    return runBlocking {
+        treeRows
+            .allPositions
+            .count { position -> treeRows.isTreeVisible(position) }
+    }
 }
 
 fun part2(input: List<String>) = input.size
+
+typealias Forest = List<List<Int>>
+
+private suspend fun Forest.isTreeVisible(position: Position) =
+    Direction.entries.any { direction ->
+        positions(from = position, inDirection = direction)
+            .map { otherPosition -> this[otherPosition]!! < this[position]!! }
+            .firstOrNull { isShorter -> !isShorter }== null
+    }
+
+private fun Forest.positions(
+    from: Position,
+    inDirection: Direction
+) = flow {
+    var here = from move inDirection
+    while (isPositionValid(here)) {
+        emit(here)
+        here = here move inDirection
+    }
+}
+
+private val Forest.allPositions
+    get() = flow {
+        forEachIndexed { rowIndex, row ->
+            row.indices.forEach { columnIndex ->
+                emit(Position(row = rowIndex, col = columnIndex))
+            }
+        }
+    }
+
+private fun Forest.isPositionValid(position: Position) =
+    with(position) { row in indices && col in this@isPositionValid[row].indices }
+
+private operator fun Forest.get(position: Position) =
+    if (isPositionValid(position)) this[position.row][position.col]
+    else null
+
+private data class Position(val row: Int, val col: Int)
+
+private enum class Direction(val rowDelta: Int, val colDelta: Int) {
+    North(rowDelta = -1, colDelta = 0),
+    South(rowDelta = 1, colDelta = 0),
+    East(rowDelta = 0, colDelta = 1),
+    West(rowDelta = 0, colDelta = -1);
+}
+
+private infix fun Position.move(direction: Direction) = Position(
+    row = row + direction.rowDelta,
+    col = col + direction.colDelta
+)
