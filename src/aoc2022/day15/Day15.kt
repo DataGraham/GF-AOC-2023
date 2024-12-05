41d8c08d17fc529dedaf153450c932bfb7f2b2b7
Graham First <graham.first@gmail.com>
2024-12-04 22:08:04 -0500

    Solved part 2!

--- src/aoc2022/day15/Day15.kt
+++ src/aoc2022/day15/Day15.kt
@@ -19,20 +19,36 @@ fun main() {
     check(part1(testInput, row = 10).also { it.println() } == 26)
     println("Part 1 Answer: ${part1(input, row = 2000000)}")
 
-    //check(part2(testInput).also { it.println() } == 93)
-    //println("Part 2 Answer: ${part2(input)}")
+    check(part2(testInput, maxCoordinate = 20, multiplier = 4000000).also { it.println() } == 56000011L)
+    println("Part 2 Answer: ${part2(input, maxCoordinate = 4000000, multiplier = 4000000)}")
 }
 
-fun part1(input: List<String>, row: Int): Int {
-    return impossibleBeaconCount(input, row = row)
-}
+fun part1(input: List<String>, row: Int) =
+    impossibleBeaconCount(input, row = row)
 
 // TODO: Will need to optimize for part 2
 //  Using https://github.com/jonpeterson/kotlin-range-sets perhaps?!
 //  Although it's not optimized with a binary or interpolation search for the relevant overlapping range.
 //  Keep in mind that beacon positions are always within the ruled-out range
 //  and don't need to be ruled-in, as we're explicitly looking for an undetected beacon!
-fun part2(input: List<String>) = input.size
+fun part2(input: List<String>, maxCoordinate: Int, multiplier: Int) =
+    sensorReadingParser
+        .parseLines(input)
+        .undetectedBeaconPosition(maxCoordinate = maxCoordinate)
+        .tuningFrequency(multiplier = multiplier)
+
+private fun List<SensorReading>.undetectedBeaconPosition(maxCoordinate: Int) =
+    (0..maxCoordinate).let { positionRange ->
+        positionRange.firstNotNullOfOrNull { row ->
+            IntRangeSet(positionRange)
+                .apply { removeAll(beaconColumnsWithinRange(row = row)) }
+                .firstOrNull()?.start?.let { beaconColumn ->
+                    Position(row = row, col = beaconColumn)
+                }
+        }!!
+    }
+
+private fun Position.tuningFrequency(multiplier: Int) = col.toLong() * multiplier + row.toLong()
 
 data class SensorReading(
     val sensorPosition: Position,
@@ -49,14 +65,23 @@ val sensorReadingParser = """(-?\d+)""".let { captureInteger ->
     }
 }
 
-fun impossibleBeaconCount(input: List<String>, row: Int) =
+private fun List<SensorReading>.beaconColumnsWithinRange(row: Int) =
+    IntRangeSet(
+        map { sensorReading ->
+            sensorReading.beaconColumnsWithinRange(row = row)
+        }
+    )
+
+private fun impossibleBeaconCount(input: List<String>, row: Int) =
     sensorReadingParser.parseLines(input).let { sensorReadings ->
         sensorReadings
-            .map { sensorReading -> sensorReading.beaconColumnsWithinRange(row = row) }
-            .let { ranges -> IntRangeSet(ranges) }
+            .beaconColumnsWithinRange(row = row)
             .apply {
-                sensorReadings.beaconColumnsInRow(targetRow = row)
-                    .forEach { beaconColumn -> remove(beaconColumn.toIntRange()) }
+                removeAll(
+                    sensorReadings
+                        .detectedBeaconColumnsInRow(targetRow = row)
+                        .map { beaconColumn -> beaconColumn.toIntRange() }
+                )
             }
             .sumOf { range -> range.size }
     }
@@ -67,7 +92,7 @@ fun SensorReading.beaconColumnsWithinRange(row: Int): IntRange {
     else ((sensorPosition.col - delta)..(sensorPosition.col + delta))
 }
 
-private fun List<SensorReading>.beaconColumnsInRow(targetRow: Int) =
+private fun List<SensorReading>.detectedBeaconColumnsInRow(targetRow: Int) =
     mapNotNull { sensorReading ->
         sensorReading.closestBeacon.run {
             col.takeIf { row == targetRow }
