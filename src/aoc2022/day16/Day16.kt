16043cd665b7924672b2226c397f2fb457de371e
Graham First <graham.first@gmail.com>
2024-12-08 17:11:40 -0500

    Attempt greedy solution to part 1
    But it doesn't produce the optimal result

--- src/aoc2022/day16/Day16.kt
+++ src/aoc2022/day16/Day16.kt
@@ -1,20 +1,21 @@
 package aoc2022.day16
 
+import Edge
 import RegexParser
+import leastPathCost
 import parseLines
-import printLines
 import println
 import readInput
 import kotlin.math.max
 
 fun main() {
-    test()
+    // test()
 
     // test if implementation meets criteria from the description, like:
-    //val testInput = readInput("aoc2022/day16/Day16_test")
-    //val input = readInput("aoc2022/day16/Day16")
+    val testInput = readInput("aoc2022/day16/Day16_test")
+    // val input = readInput("aoc2022/day16/Day16")
 
-    //check(part1(testInput).also { it.println() } == 26)
+    check(part1Greedy(testInput).also { it.println() } == 1651)
     //println("Part 1 Answer: ${part1(input)}")
 
     //check(part2(testInput).also { it.println() } == 1)
@@ -49,18 +50,81 @@ private fun testCase(expected: Int, vararg valves: Valve) = check(
 )
 
 fun part1(input: List<String>): Int {
-    val valves = valveParser.parseLines(input).associateBy { it.name }
-    valves.entries.printLines()
-
+    val valves = parseValves(input)
     val initialNode = Node(
         currentValveName = "AA",
         openedValves = emptySet(),
         elapsedMinutes = 0,
         pressureReleased = 0
     )
-
     return valves.maximumReleasablePressure(initialNode)
 }
+
+fun part1Greedy(input: List<String>): Int {
+    val valves = parseValves(input)
+    val nonZeroValves = valves.values.filter { valve -> valve.flowRate > 0 }
+    val openedValveNames = HashSet<String>()
+    var timeElapsed = 0
+    var totalEventualPressureReleased = 0
+    var currentValveName = "AA" // TODO: Or is it the first valve always?
+    while (openedValveNames.size < nonZeroValves.size && timeElapsed < 30) {
+        // Find the greediest next unopened valve with non-zero-flow (which could be current)
+        // using the shortest physical path from here to that valve.
+        val nextValveOpeningStats = nonZeroValves
+            .filter { valve -> valve.name !in openedValveNames }
+            .associateWith { destinationValve ->
+                val d = leastPathCost(
+                    start = currentValveName,
+                    edges = { valveName ->
+                        valves[valveName]!!.leadsTo.map { adjacentValveName ->
+                            Edge(
+                                destination = adjacentValveName,
+                                cost = 1
+                            )
+                        }
+                    },
+                    isEnd = { valveName ->
+                        valveName == destinationValve.name
+                    }
+                )
+
+                // Ignore if it would take too long to get to
+                if ((d + 1) <= (30 - timeElapsed)) ValveOpeningStats(
+                    distance = d,
+                    eventualPressureReleased = (30 - timeElapsed - d - 1) * destinationValve.flowRate
+                )
+                else null
+            }
+
+        // Choose the max eventual pressure released
+        val (nextValve, nextValveStats) = nextValveOpeningStats.entries.maxBy { entry ->
+            entry.value?.eventualPressureReleased ?: 0
+        }
+        if (nextValveStats == null) {
+            timeElapsed = 30
+        } else {
+            println("Opening valve ${nextValve.name} after $timeElapsed minutes")
+
+            // Mark us being at the valve
+            currentValveName = nextValve.name
+
+            // Mark the valve as opened
+            openedValveNames += nextValve.name
+
+            // Add to elapsed time
+            timeElapsed += nextValveStats.distance + 1
+
+            // Add the pressure released
+            totalEventualPressureReleased += nextValveStats.eventualPressureReleased
+        }
+    }
+    return totalEventualPressureReleased
+}
+
+private data class ValveOpeningStats(
+    val distance: Int,
+    val eventualPressureReleased: Int
+)
 
 private val valveParser =
     RegexParser("""Valve (\p{Upper}+) has flow rate=(\d+); tunnels? leads? to valves? (.*)""") { captures ->
@@ -70,6 +134,11 @@ private val valveParser =
             leadsTo = captures[2].split(", ")
         )
     }
+
+private fun parseValves(input: List<String>) =
+    valveParser
+        .parseLines(input)
+        .associateBy { valve -> valve.name }
 
 private data class Valve(
     val name: String,
@@ -114,6 +183,36 @@ private fun Map<String, Valve>.maximumReleasablePressure(startNode: Node): Int =
 
 // TODO: Or is there some sort of graph path maximization algorithm? Like a "longest" or "most cost" path?
 //  There is! -> https://en.wikipedia.org/wiki/Longest_path_problem
+
+// TODO: To simplify the graph, find all of the shortest pair-wise paths in terms of simple distance between valves.
+//  Then, we can consider a meta-graph where each node has a valve and a set of open valves,
+//  where from each node, there are edges leading to, for each unopened valve,
+//  a state where we are at that valve and it is now opened,
+//  with a negative cost equal to: (D + 1) x F
+//
+//  Where D equals the physical distance of the shortest path to that valve,
+//  and F equals the sum of the flow rates of the already-opened valves.
+//
+//  Basically, what we're saying is that the only sensible move is to proceed via the shortest path
+//  to an unopened valve and open it.
+//
+// But, two things:
+//
+//  1. We must limit the total time T = (sum of D for all valve-to-valve traversals) + (number of valves opened)
+//
+//  2. We also need to be able, in the case that all valves are opened, to jump to t = 30 minutes,
+//      with a negative cost of (30 - t) x F
+//
+// In both cases, it feels like I need to include the elapsed time, and this is going to blow-up the size of the graph!
+
+// TODO: At the end of the day, the only possibilities we're really considering is the different orders in which to open
+//  all of the valves (or some subset of them as time allows).
+//
+//  But for V valves that's O(V!)
+//
+//  Is there a greedy algorithm somehow, like can I somehow, at each step, pick the best available trade-off
+//  for any next valve, between the time it takes to get there, vs. the total pressure that will then be released
+//  in the rest of the time?
 
 // The next nodes are:
 // IFF elapsed minutes < 30:
