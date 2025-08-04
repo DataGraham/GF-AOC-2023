c17ff330cff97660b85a210a8dc4008d4ac27b49
Graham First <graham.first@gmail.com>
2025-08-04 19:42:04 -0400

    Solve part 2 example

--- src/aoc2024/day01/Day_01.kt
+++ src/aoc2024/day01/Day_01.kt
@@ -10,23 +10,29 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2024/day01/Day01_test")
     check(part1(testInput).also { it.println() } == 11)
-    //check(part2(testInput).also { it.println() } == 1)
+    check(part2(testInput).also { it.println() } == 31)
 
     val input = readInput("aoc2024/day01/Day01")
     println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Int {
-    val locationIdPairs = locationIdPairParser.parseLines(input)
-    val listA = locationIdPairs.map { it.first }.sorted()
-    val listB = locationIdPairs.map { it.second }.sorted()
-    return listA.zip(listB) { a, b -> abs(a - b) }.sum()
-}
+fun part1(input: List<String>) =
+    input.parseLocationIdLists().let { (listA, listB) ->
+        listA.sorted().zip(listB.sorted()) { a, b -> abs(a - b) }.sum()
+    }
 
 fun part2(input: List<String>): Int {
-    return input.size
+    val (listA, listB) = input.parseLocationIdLists()
+    return listA.sumOf { a ->
+        a * listB.count { b -> b == a }
+    }
 }
+
+private fun List<String>.parseLocationIdLists() =
+    locationIdPairParser.parseLines(this).let { locationIdPairs ->
+        locationIdPairs.map { it.first } to locationIdPairs.map { it.second }
+    }
 
 private val locationIdPairParser by lazy {
     RegexParser("""(\d+) +(\d+)""") { (locationIdA, locationIdB) ->
