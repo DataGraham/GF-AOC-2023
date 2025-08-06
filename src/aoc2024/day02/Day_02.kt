741303deff5f327b4e6d623c569576829305d268
Graham First <graham.first@gmail.com>
2025-08-05 21:32:35 -0400

    Solve part 2

--- src/aoc2024/day02/Day_02.kt
+++ src/aoc2024/day02/Day_02.kt
@@ -7,32 +7,37 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2024/day02/Day02_test")
     check(part1(testInput).also { it.println() } == 2)
-    //check(part2(testInput).also { it.println() } == 1)
+    check(part2(testInput).also { it.println() } == 4)
 
     val input = readInput("aoc2024/day02/Day02")
     println("Part 1 Answer: ${part1(input)}")
-    //println("Part 2 Answer: ${part2(input)}")
+    println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>) = input.parseReports().count { report -> report.isSafeReport }
 
-fun part2(input: List<String>): Int {
-    return input.size
-}
+fun part2(input: List<String>) = input.parseReports().count { report -> report.isSafeReportIfDampened }
 
 private fun List<String>.parseReports() = map { line ->
     line.split(" ").map { level -> level.toInt() }
 }
 
-private val List<Int>.isSafeReport get() = deltas.isSafeDeltas
+private val Collection<Int>.isSafeReportIfDampened
+    get() = isSafeReport ||
+        indices.any { indexToDampen ->
+            filterIndexed { index, _ -> index != indexToDampen }.isSafeReport
+        }
 
-private val List<Int>.deltas get() = zipWithNext { a, b -> b - a }
+private val Collection<Int>.isSafeReport get() = deltas.isSafeDeltas
 
-private val List<Int>.isSafeDeltas get() =
-    safeRanges.any { safeRange -> safeRange.containsAll(this) }
+private val Collection<Int>.deltas get() = zipWithNext { a, b -> b - a }
+
+private val Collection<Int>.isSafeDeltas
+    get() =
+        safeRanges.any { safeRange -> safeRange.containsAll(this) }
 
 private val smallIncrease = 1..3
 private val smallDecrease = -3..-1
 private val safeRanges = listOf(smallIncrease, smallDecrease)
 
-private fun IntRange.containsAll(values: List<Int>) = values.all { it in this }
+private fun IntRange.containsAll(values: Collection<Int>) = values.all { it in this }
