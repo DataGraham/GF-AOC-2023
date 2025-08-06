c30a074eee533e64455d4ea7a0f60312d64eb92b
Graham First <graham.first@gmail.com>
2025-08-05 22:05:10 -0400

    [Day 03] Setup for day 3

--- src/aoc2024/day03/Day_03.kt
+++ src/aoc2024/day03/Day_03.kt
@@ -1,16 +1,15 @@
-/*
-package aoc2024.dayXX
+package aoc2024.day03
 
 import println
 import readInput
 
 fun main() {
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("aoc2024/dayXX/DayXX_test")
+    val testInput = readInput("aoc2024/day03/Day03_test")
     check(part1(testInput).also { it.println() } == 161)
     //check(part2(testInput).also { it.println() } == 1)
 
-    val input = readInput("aoc2024/dayXX/DayXX")
+    val input = readInput("aoc2024/day03/Day03")
     println("Part 1 Answer: ${part1(input)}")
     println("Part 2 Answer: ${part2(input)}")
 }
@@ -22,5 +21,3 @@ fun part1(input: List<String>): Int {
 fun part2(input: List<String>): Int {
     return input.size
 }
-
-*/
