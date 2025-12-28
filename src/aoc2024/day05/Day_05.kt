3371164fa0cce55facd2255b650d59c474b1b7d5
Graham First <graham.first@gmail.com>
2025-12-27 20:10:27 -0500

    TODO ideas for part 2

--- src/aoc2024/day05/Day_05.kt
+++ src/aoc2024/day05/Day_05.kt
@@ -19,7 +19,7 @@ fun main() {
 
     val input = readInput("aoc2024/day05/Day05")
     println("Part 1 Answer: ${part1(input)}")
-    println("Part 2 Answer: ${part2(input)}")
+    //println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
@@ -37,6 +37,16 @@ private fun Update.followsRules(rules: List<Rule>) =
     rules.none { rule -> breaks(rule) }
 
 fun part2(input: List<String>): Int {
+    /* TODO: Sum middle-elements of rule-breaking updates, AFTER having corrected each of their page orders.
+        If A|B and B|C, but not explicitly A|C, then we might not be giving enough of an "order" for sorting,
+        because we'd claim A and C are equivalent, when they actually aren't (well, IF B appears).
+        So, we might have to link together rules into chains, where for each rule, we link to other rules
+        that share the same "earlier" or "later" page number.
+        Then, we could start with any rule and label that 0, and then go through "earlier" and "later" rules
+        labelling them -1, -2... and 1, 2...
+        Then, when comparing for sort, we find whether there is a rule chain that includes both page numbers
+        and compare there indices within that rule chain?!
+     */
     return input.size
 }
 
