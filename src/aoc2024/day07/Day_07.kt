42da197418792504320c6fa299bf8faf1ccc201e
Graham First <graham.first@gmail.com>
2026-01-11 22:02:29 -0500

    Pass the test case, but fail the real input because inputs are too large

--- src/aoc2024/day07/Day_07.kt
+++ src/aoc2024/day07/Day_07.kt
@@ -2,28 +2,28 @@ package aoc2024.day07
 
 import RegexParser
 import parseLines
-import printLines
 import println
 import readInput
 
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2024/day07/Day07_test")
-    check(part1(testInput).also { it.println() } == 161)
+    check(part1(testInput).also { it.println() } == 3749)
     //check(part2(testInput).also { it.println() } == 1)
 
-    //val input = readInput("aoc2024/day07/Day07")
-    //println("Part 1 Answer: ${part1(input)}")
+    val input = readInput("aoc2024/day07/Day07")
+    println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Int {
-    val calibrationEquations = calibrationEquationParser
-        .parseLines(input)
-        .printLines()
-    operatorSets(length = 3).toList().printLines()
-    return input.size
-}
+fun part1(input: List<String>) =
+    calibrationEquationParser.parseLines(input)
+        .filter { equation -> equation.couleBeTrue }
+        .sumOf { equation -> equation.result }
+
+private val CalibrationEquation.couleBeTrue
+    get() = operatorSets(length = inputs.size - 1)
+        .any { operatorSet -> operatorSet.applyToInputs(inputs) == result }
 
 fun part2(input: List<String>): Int {
     return input.size
@@ -33,6 +33,19 @@ data class CalibrationEquation(
     val result: Int,
     val inputs: List<Int>
 )
+
+val calibrationEquationParser by lazy {
+    RegexParser("""(\d+):(( \d+)+)""") { captures ->
+        val (resultString, inputsString) = captures
+        CalibrationEquation(
+            result = resultString.toInt(),
+            inputs = inputsString
+                .trim()
+                .split(" ")
+                .map { it.toInt() }
+        )
+    }
+}
 
 enum class Operator(private val calculate: (Int, Int) -> Int) {
     Addition({ a, b -> a + b }),
@@ -56,15 +69,8 @@ fun operatorSets(length: Int) = generateSequence(
     }
 }
 
-val calibrationEquationParser by lazy {
-    RegexParser("""(\d+):(( \d+)+)""") { captures ->
-        val (resultString, inputsString) = captures
-        CalibrationEquation(
-            result = resultString.toInt(),
-            inputs = inputsString
-                .trim()
-                .split(" ")
-                .map { it.toInt() }
-        )
-    }
-}
+fun List<Operator>.applyToInputs(inputs: List<Int>) =
+    zip(inputs.drop(1))
+        .fold(inputs.first()) { acc, (operator, input) ->
+            operator(acc, input)
+        }
