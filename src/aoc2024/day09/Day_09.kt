3fef768b4d3b92386d15d6f3af06e7e2b79c1b01
Graham First <graham.first@gmail.com>
2026-01-18 18:59:10 -0500

    Solve part 1

--- src/aoc2024/day09/Day_09.kt
+++ src/aoc2024/day09/Day_09.kt
@@ -6,27 +6,29 @@ import readInput
 fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("aoc2024/day09/Day09_test")
-    check(part1(testInput).also { it.println() } == 1928)
+    check(part1(testInput).also { it.println() } == 1928L)
     //check(part2(testInput).also { it.println() } == 1)
 
-    //val input = readInput("aoc2024/day09/Day09")
-    //println("Part 1 Answer: ${part1(input)}")
+    val input = readInput("aoc2024/day09/Day09")
+    println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${part2(input)}")
 }
 
-fun part1(input: List<String>): Int {
+fun part1(input: List<String>): Long {
     val codes = input.first().toCharArray().map { it.digitToInt() }
-    val disk = codes.flatMapIndexed { i, c ->
-        if (i % 2 == 0) {
-            // start with a file
-            val fileId = i / 2
-            val fileSize = c
-            List(fileSize) { fileId }
-        } else {
-            List(c) { null }
-        }
+    val disk = codes.flatMapIndexed { index, code ->
+        if (index % 2 == 0)
+            // Start with a file of length <code> containing the file id (index)
+            List(code) { index / 2 }
+        else
+            // Next is empty space of length <code>
+            List(code) { null }
     }.toTypedArray()
     defrag(disk)
+    return disk.asIterable().checksum()
+}
+
+fun part2(input: List<String>): Int {
     return input.size
 }
 
@@ -41,6 +43,5 @@ private fun defrag(disk: Array<Int?>) {
     }
 }
 
-fun part2(input: List<String>): Int {
-    return input.size
-}
+private fun Iterable<Int?>.checksum() =
+    asSequence().mapIndexed { index, i -> index * (i ?: 0).toLong() }.sum()
