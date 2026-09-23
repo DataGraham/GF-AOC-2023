26f1d843eaa9fed536605bde141963aadd22337d
Graham First <graham.first@gmail.com>
2026-09-23 18:12:25 -0400

    Solve part 1

--- src/aoc2024/day10/Day_10.kt
+++ src/aoc2024/day10/Day_10.kt
@@ -1,5 +1,11 @@
 package aoc2024.day10
 
+import Direction
+import Position
+import allPositions
+import get
+import isPositionValid
+import move
 import println
 import readInput
 
@@ -9,15 +15,45 @@ fun main() {
     check(part1(testInput).also { it.println() } == 36)
     //check(part2(testInput).also { it.println() } == 1)
 
-    //val input = readInput("aoc2024/day10/Day10")
-    //println("Part 1 Answer: ${part1(input)}")
+    val input = readInput("aoc2024/day10/Day10")
+    println("Part 1 Answer: ${part1(input)}")
     //println("Part 2 Answer: ${part2(input)}")
 }
 
 fun part1(input: List<String>): Int {
-    return input.size
+    val heights = input.map { line -> line.map { it.digitToInt() } }
+    val trailheads = heights
+        .allPositions()
+        .filter { position -> heights[position] == 0 }
+    return trailheads.sumOf { trailhead -> heights.trailScore(trailhead = trailhead) }
 }
 
 fun part2(input: List<String>): Int {
     return input.size
+}
+
+fun List<List<Int>>.trailScore(trailhead: Position): Int {
+    require(this[trailhead] == 0)
+    return reachableNines(trailhead)
+        //        .also {
+        //            println("Reachable 9s from $trailhead are $it")
+        //        }
+        .size
+}
+
+fun List<List<Int>>.reachableNines(startPosition: Position): Set<Position> {
+    val heightHere = this[startPosition]
+    val nextHeight = heightHere + 1
+    val validNextPositions = Direction
+        .orthogonal
+        .map { direction -> startPosition move direction }
+        .filter { nextPosition -> isPositionValid(nextPosition) && this[nextPosition] == nextHeight }
+    return if (heightHere == 8)
+        validNextPositions.toSet()
+    else validNextPositions
+        .flatMap { validNextPosition -> reachableNines(startPosition = validNextPosition) }
+        .toSet()
+    //        .also {
+    //            println("Reachable 9s from $startPosition are $it")
+    //        }
 }
